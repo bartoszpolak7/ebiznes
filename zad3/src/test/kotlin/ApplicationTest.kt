4c8c5455fc794bcf4721a399dd9ebe51d8df11de
ratman <szczuras7@proton.me>
2026-04-12 00:37:10 +0200

    zad3 3.0
    zad3 3.0

---
+++ zad3/src/test/kotlin/ApplicationTest.kt
@@ -0,0 +1,22 @@
+package io.github.siemamen7
+
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+import io.github.siemamen7.routing.configureRouting
+
+class ApplicationTest {
+    @Test
+    fun testPostWebhookSend() = testApplication {
+        application {
+            configureRouting()
+        }
+        val response = client.post("/webhook/send") {
+            header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+            setBody("{\"message\":\"Hello Discord\"}")
+        }
+        // This is expected to fail with UnsupportedMediaType or ContentTransformationException because server lacks ContentNegotiation
+        println("[DEBUG_LOG] Status: ${response.status}")
+    }
+}
